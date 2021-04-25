0b9e9757b69100c33fa145e8dcdd5f01b5d90b48
at-somenone <yakowlev.ig@yandex.com>
2021-04-25 13:26:11 +0300

    meow

---
+++ Archlab555/Archlab555/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Archlab555.net.yandex.speller;
+
+namespace Archlab555
+{
+    class Program
+    {
+        private static void Main(string[] args) {
+            var hhhh = new SpellService();
+            Console.Write("Введите строку, которую нужно проверить: ");
+            var str = Console.ReadLine().Replace("\n", "");
+            var yeahhh = hhhh.checkText(str, "ru", 1, "");
+            var lastIndex = 0;
+            ;
+            foreach (var idk in yeahhh) {
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write(str.Substring(lastIndex, idk.col - lastIndex));
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(str.Substring(idk.col, idk.len));
+                lastIndex = idk.col + idk.len;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine(str.Substring(lastIndex));
+        }
+    }
+}
